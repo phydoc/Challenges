804fcb36fa703a78524bc19b5209d76b9830671e
phydoc <phydoc@protonmail.com>
2020-05-19 11:53:17 +0200

    Added url to the seven

--- sevenBoom.js
+++ sevenBoom.js
@@ -17,3 +17,5 @@ const sevenBoom = (arr) => {
 console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]))
 console.log(sevenBoom([8, 6, 33, 100]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
+
+// https://edabit.com/challenge/6R6gReGTGwzpwuffD
